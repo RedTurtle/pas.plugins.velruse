1c81bb2621becd084be7da8bc925c2d40d2431ba
Luca Fabbri <luca@keul.it>
2013-06-13 15:46:11 +0200

    moved import of logger, for import order problem

--- pas/plugins/velruse/__init__.py
+++ pas/plugins/velruse/__init__.py
@@ -4,9 +4,10 @@ import logging
 
 from AccessControl.Permissions import add_user_folders
 from Products.PluggableAuthService import registerMultiPlugin
-from .plugin import VelruseUsers, AddForm
 
 logger = logging.getLogger('pas.plugins.velruse')
+
+from pas.plugins.velruse.plugin import VelruseUsers, AddForm
 
 #registerMultiPlugin(VelruseUsers.meta_type)
 
